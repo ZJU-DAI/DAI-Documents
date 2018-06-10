40d3715ae46700a76d398f1f4ce27e72f2d9daf9
Huang Xiaojie <hxj_0057@qq.com>
2018-06-10 23:21:42 +0800

    Add files via upload

--- image forgery/hxj/CFA/prediction.m
+++ image forgery/hxj/CFA/prediction.m
@@ -4,14 +4,10 @@
 function [pred_error] = prediction(im)
 
 % predictor with a bilinear kernel 拉普拉斯核
-Hpred = [ 0,   1,    0;
-          1,  -4,    1;
-          0,   1,    0 ];
+Hpred = [ 0,   -1,    0;
+          -1,  4,    -1;
+          0,   -1,    0 ];
       
-% imfilter 对任意类型的数组或多维图像进行滤波 
-% replicate 图像大小通过复制外边界的值来扩展
-
-pred_error = imfilter(double(im),double(Hpred),'replicate');
-
+pred_error = imfilter(double(im),double(Hpred));
 
 return
